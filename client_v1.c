bc5aa4cf73527e5e1847c7925ff62477125ffea4
Ratatinator97 <asidiras.csi@gmail.com>
2019-11-08 18:26:39 +0100

    Différents codes ajotués... Debug pas fini

---
+++ client_v1.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+
+#define RCVSIZE 508
+
+
+int main (int argc, char *argv[]) {
+    
+    struct sockaddr_in adresse;
+    FILE *file;
+    int port,port2 = 5001;
+    char msg[RCVSIZE];
+    char buffer_ecriture[1024];
+
+    if(argc > 3){
+        printf("Too many arguments\n");
+        exit(0);
+    }
+    if(argc < 3){
+        printf("Argument expected\n");
+        exit(0);
+    }
+    if(argc == 3){
+        port = atoi(argv[2]);
+    }
+    
+    int server_desc = socket(AF_INET, SOCK_DGRAM, 0);
+    memset(&adresse, 0, sizeof(adresse));
+    if (server_desc < 0) {
+        perror("cannot create socket\n");
+        return -1;
+    }
+        
+    adresse.sin_family= AF_INET;
+    adresse.sin_port= htons(port);
+    adresse.sin_addr.s_addr= inet_addr(argv[1]);
+  
+
+    socklen_t len = sizeof(adresse);
+    int  n; 
+
+    printf("SYNC...\n");
+    strcpy(msg, "SYN");
+    
+    sendto(server_desc,(const char*)msg, strlen(msg),MSG_CONFIRM, (const struct sockaddr *) &adresse,sizeof(adresse));
+    printf("sending SYN\n");
+    n = recvfrom(server_desc, (char *)msg, RCVSIZE,MSG_WAITALL, (struct sockaddr *) &adresse,&len);
+    printf("%d\n",ntohs(adresse.sin_port));
+    msg[n] = '\0';
+        
+    char *token;
+    token = strtok(msg, "SYN-ACK");
+    if(token != NULL){
+        port2 =  atoi(token);
+        printf("Le port lu dans syn ack est : %d\n",port2);
+        printf("SYN-ACK received\n");
+        strcpy(msg, "ACK");
+        sendto(server_desc,(const char*)msg, strlen(msg),MSG_CONFIRM, (const struct sockaddr *) &adresse,sizeof(adresse));
+        printf("sending ack, connected\n");
+    }else{
+        printf("bad syn-ack\n");
+        exit(0);
+    }
+    adresse.sin_port = htons(port2);
+    int fini = 0;
+    char nom_fichier[20];
+    char num_seq_s[7];
+    int num_seq;
+    char ack_s[11];
+    while(1){
+
+        fgets(msg, RCVSIZE, stdin);
+        strcpy(msg,strtok(msg, "\n"));
+        printf("Le msg lu est : %s...\n",msg);
+        strcpy(nom_fichier,msg);
+        printf("Le nom du fichier est : %s...\n",nom_fichier);
+        sendto(server_desc,(const char*)msg, strlen(msg),MSG_CONFIRM, (const struct sockaddr *) &adresse,sizeof(adresse)); 
+        printf("the value of sent is:%lu\n", strlen(msg));
+        fini = 0;
+        num_seq = 0;
+        while(fini == 0){
+            
+            n = recvfrom(server_desc, (char *)msg, RCVSIZE,MSG_WAITALL, (struct sockaddr *) &adresse,&len);
+            msg[n]='\0';
+            printf("Reception octet no : %d\n",num_seq);
+            if(strcmp(msg,"FIN")==0){
+                fini = 1;
+            	if((file=fopen(nom_fichier,"w"))==NULL){
+		            //if the file does not exist print the string
+		            printf("Cannot open the file...");
+		            exit(1);
+	            }
+	            //write the values on the file
+	            fwrite(buffer_ecriture,sizeof(char),10000000,file);
+	            //close the file
+	            fclose(file);    
+            }else{
+                strcpy(ack_s,"ACK_");
+                memcpy(num_seq_s,msg,6);
+                num_seq = atoi(num_seq_s);
+                memcpy(buffer_ecriture+(RCVSIZE-6)*num_seq,msg,RCVSIZE-6);
+
+                strcat(ack_s,num_seq_s);
+                strcpy(msg, ack_s);   
+                printf("Acquitement : %s , no : %d\n",msg,num_seq);             
+                sendto(server_desc,(const char*)msg, strlen(msg),MSG_CONFIRM, (const struct sockaddr *) &adresse,sizeof(adresse));
+            }
+        }
+
+
+    }
+
+    return 0;
+
+}
---
+++ serveur_v1.c
@@ -0,0 +1,198 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <arpa/inet.h>
+
+#define RCVSIZE 508
+
+int main (int argc, char *argv[]) {
+
+    struct sockaddr_in adresse, cliaddr, adresse2; 
+    memset(&cliaddr, 0, sizeof(cliaddr)); 
+    memset(&adresse, 0, sizeof(adresse)); 
+    memset(&adresse2, 0, sizeof(adresse)); 
+    
+    
+    int port_udp, port_udp2= 5002;   
+
+    char buffer[RCVSIZE]; 
+
+
+    if(argc > 2){
+        printf("Too many arguments\n");
+        exit(0);
+    }
+    if(argc < 2){
+        printf("Argument expected\n");
+        exit(0);
+    }
+    if(argc == 2){
+        int n = 1;
+        port_udp = atoi(argv[1]);
+        port_udp2 = atoi(argv[1])+n;
+        n++;
+        printf("%d\n",port_udp);
+    }
+
+    
+
+    int server_desc_udp = socket(AF_INET, SOCK_DGRAM, 0);
+    if(server_desc_udp < 0){
+        perror("Cannot create udp socket\n");
+        return -1;
+    }
+    
+    adresse.sin_family= AF_INET;
+    adresse.sin_port= htons(port_udp);    
+    adresse.sin_addr.s_addr= INADDR_ANY;
+
+    adresse2.sin_family= AF_INET;
+    adresse2.sin_port= htons(port_udp2);    
+    adresse2.sin_addr.s_addr= INADDR_ANY;
+
+    if (bind(server_desc_udp, (struct sockaddr*) &adresse, sizeof(adresse))<0) {
+        perror("Bind failed\n");
+        close(server_desc_udp);
+        return -1;
+    }
+
+    socklen_t len = sizeof(cliaddr);
+    int n;
+    n = recvfrom(server_desc_udp, (char *)buffer, RCVSIZE,MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len);
+    buffer[n] = '\0';
+    
+    if(strcmp(buffer,"SYN")==0){
+        char synack[13];
+        char port_udp2_s[6];
+        strcpy(synack, "SYN-ACK");
+
+
+        printf("SYN COMING\n");
+        snprintf((char *) port_udp2_s, 10 , "%d", port_udp2 ); 
+        strcat(synack,port_udp2_s);
+        strcpy(buffer, synack);
+
+        // En multi client il faut creer le thread avant de envoyer le synack
+        // faire lecture fichier -> envoi -> réecriture
+        // 
+
+        sendto(server_desc_udp, (char *)buffer, strlen(buffer),MSG_CONFIRM, (const struct sockaddr *) &cliaddr, len);
+        printf("Waiting for ACK...\n");
+        n = recvfrom(server_desc_udp, (char *)buffer, RCVSIZE,MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len);
+        buffer[n] = '\0';
+        if(strcmp(buffer,"ACK")==0){
+        printf("ACK received, connection\n");
+
+        
+        // Handshake reussi !
+
+        }else{
+        printf("Bad ack");
+        exit(0);
+        }
+    }else{
+        printf("bad SYN");
+        exit(0);
+    }
+
+    int server_desc_udp2 = socket(AF_INET, SOCK_DGRAM, 0);
+    if (bind(server_desc_udp2, (struct sockaddr*) &adresse2, sizeof(adresse2))<0) {
+        perror("Bind failed\n");
+        close(server_desc_udp);
+        exit(0);
+    }
+
+
+    FILE* fichier;
+    
+    while(1) {
+
+
+        printf("WAITING FOR MESSAGE\n");
+        
+        n = recvfrom(server_desc_udp2, (char *)buffer, RCVSIZE,MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len); 
+        buffer[n] = '\0';
+
+        printf("On a recu le msg : %s ...\n", buffer);
+        // POSER UN ACK ICI
+        
+        fichier = NULL;
+  
+        if((fichier=fopen(buffer,"r"))==NULL){
+
+            printf("Something's wrong I can feel it (file)\n");
+            exit(1);
+        }
+
+        size_t pos = ftell(fichier);    // Current position
+        fseek(fichier, 0, SEEK_END);    // Go to end
+        size_t length = ftell(fichier); // read the position which is the size
+        fseek(fichier, pos, SEEK_SET);
+        char buffer_lecture[length];
+
+        if((fread(buffer_lecture,sizeof(char),length,fichier))!=length){
+            printf("Something's wrong I can feel it (read)....\n");
+        }
+        fclose(fichier);
+        
+        printf("Le fichier lu a une taille de %d octets\n",length);
+
+        int nb_morceaux;
+
+        if((length % (RCVSIZE-6)) != 0){
+            nb_morceaux = (length/(RCVSIZE-6))+1;
+        } else {
+            nb_morceaux = length/(RCVSIZE-6);
+        }        
+        
+        printf("Le fichier lu est decoupe en %d envois\n",nb_morceaux);
+
+        int ack = 0;
+        char num_seq_tot[7];
+        char num_seq_s[7];
+        
+        for(int num_seq=0;num_seq < nb_morceaux;num_seq++){
+            printf("Numero de sequence %d\n",num_seq);
+            memcpy(buffer+6, buffer_lecture+((RCVSIZE-6)*num_seq), RCVSIZE);
+            
+            strcpy(num_seq_tot, "000000");
+            snprintf((char *) num_seq_s, 10 , "%d", num_seq ); 
+            for(int i = strlen(num_seq_s);i>=0;i--){
+                num_seq_tot[strlen(num_seq_tot)-i]=num_seq_s[strlen(num_seq_s)-i];
+            }
+            memcpy(buffer,num_seq_tot, 6);
+            
+            ack = 0;
+            while(ack == 0){
+                
+                
+                sendto(server_desc_udp2,(const char*)buffer, strlen(buffer),MSG_CONFIRM, (const struct sockaddr *) &cliaddr,len);
+                
+                n = recvfrom(server_desc_udp2, (char *)buffer, RCVSIZE,MSG_WAITALL, (struct sockaddr *) &cliaddr,&len); 
+                buffer[n] = '\0';  
+
+                
+                if(atoi(strtok(buffer,"ACK_")) == num_seq){
+                    ack = 1;
+                    printf("Acquitement de %d est reussi\n",num_seq);
+                }
+                
+            }
+        }
+        
+        // ToDo retransmission si pas ack
+        sendto(server_desc_udp2,(char*)"FIN", strlen("FIN"),MSG_CONFIRM, (const struct sockaddr *) &adresse,sizeof(adresse));
+        printf("WAITING for message final ack\n");
+        n = recvfrom(server_desc_udp2, (char *)buffer, RCVSIZE,MSG_WAITALL, (struct sockaddr *) &adresse,&len); 
+        buffer[n] = '\0';
+
+    }        
+    
+    return 0;
+}
